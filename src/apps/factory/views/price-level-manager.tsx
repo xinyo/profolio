6dea9500be113e5dc05082036a318add4f7fbd0d
xinyo <xinyoc@gmail.com>
2026-07-10 16:20:24 +1000

    style

--- src/apps/factory/views/price-level-manager.tsx
+++ src/apps/factory/views/price-level-manager.tsx
@@ -1,5 +1,5 @@
-import { useTranslation } from "react-i18next";
 import devImage from "@/assets/dev.webp";
+import { useTranslation } from "react-i18next";
 
 export function PriceLevelManagerView() {
   const { t } = useTranslation();
@@ -7,7 +7,7 @@ export function PriceLevelManagerView() {
   return (
     <section className="factory-view">
       <h2>{t("factory.views.priceLevelManager.title")}</h2>
-      <div className="flex flex-col">
+      <div className="flex flex-col items-center">
         <div className="factory-view__dev-image mt-8">
           <img
             src={devImage}
