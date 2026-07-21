1754b84c80afc5c63f26ab00a30c796382f36790
xinyo <xinyoc@gmail.com>
2026-07-22 01:30:04 +1000

    fix typo

--- src/hooks/use-page-title.ts
+++ src/hooks/use-page-title.ts
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 
 export function usePageTitle(title: string) {
   useEffect(() => {
-    document.title = `${title} — profolio`;
+    document.title = `${title} — portfolio`;
   }, [title]);
 }
